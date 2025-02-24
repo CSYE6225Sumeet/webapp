15bacfa2e4b91b653485e0861ad4b7ad1e18ec88
Sumeet <rane.su@northeastern.edu>
2025-02-23 20:19:03 -0500

    testing ci

--- src/packer/scripts/install-script.sh
+++ src/packer/scripts/install-script.sh
@@ -7,7 +7,20 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y unzip
 sudo apt-get install -y mysql-server
 sudo systemctl enable --now mysql
-sudo mysql -e "CREATE DATABASE $DB_NAME;"
+
+sudo mysql --user=root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+echo "Setting up MySQL database and user..."
+sudo mysql -uroot -p$DB_PASSWORD <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+FLUSH PRIVILEGES;
+EOF
+
+# sudo mysql -e "CREATE DATABASE $DB_NAME;"
+
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo node -v
