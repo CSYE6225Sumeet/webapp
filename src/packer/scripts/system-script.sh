86b98b5773882e37212b4fb76f6a13fb534f37fc
Sumeet <rane.su@northeastern.edu>
2025-03-18 18:11:00 -0400

    removed mysql installation & configuration

--- src/packer/scripts/system-script.sh
+++ src/packer/scripts/system-script.sh
@@ -5,21 +5,24 @@ sudo useradd csye6225 --shell /usr/sbin/nologin -g csye6225
 sudo cp /tmp/webapp.service /etc/systemd/system/
 sudo cp /tmp/webapp.zip /opt/
 sudo unzip /opt/webapp.zip -d /opt/webapp
-cd /opt/webapp/src
 
-env_values=$(cat <<END
-DB_NAME=$DB_NAME
-DB_USER=$DB_USER
-DB_PASSWORD=$DB_PASSWORD
-DB_HOST=$DB_HOST
+#--------------------------------------
+# cd /opt/webapp/src
 
-END
-)
+# env_values=$(cat <<END
+# DB_NAME=$DB_NAME
+# DB_USER=$DB_USER
+# DB_PASSWORD=$DB_PASSWORD
+# DB_HOST=$DB_HOST
 
-echo "$env_values" | sudo tee .env >/dev/null
+# END
+# )
+
+# echo "$env_values" | sudo tee .env >/dev/null
 
 sudo chown -R csye6225:csye6225 /opt/webapp
-sudo chown csye6225:csye6225 .env
+# sudo chown csye6225:csye6225 .env
+#-------------------------------------------------
 sudo npm install
 sudo chown csye6225:csye6225 node_modules
 
