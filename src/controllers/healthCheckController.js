456d9ad7a1e12fae335078849663e8dff182a012
Sumeet <rane.su@northeastern.edu>
2025-03-26 01:43:15 -0400

    Added logger for 400 and 405 status codes

--- src/controllers/healthCheckController.js
+++ src/controllers/healthCheckController.js
@@ -1,16 +1,21 @@
 const healthCheckService = require('../services/healthCheckService');
+const logger = require('../utils/logger');
 
 const healthCheck = async (req, res) => {
   try {
+    logger.info('Health check endpoint called');
     await healthCheckService.performHealthCheck();
+    logger.info('Health check successful');
     res.status(200).send();
   } catch (error) {
+    logger.error('Health check failed', { error: error.message, stack: error.stack });
     res.status(503).send();
   }
 };
 
 const methodNotAllowed = (req, res) => {
-  res.status(405).send(); // 405 Method Not Allowed
+  logger.warn(`Method not allowed on health check route: ${req.method}`);
+  res.status(405).send();
 };
 
 module.exports = { healthCheck, methodNotAllowed };
--- src/middleware/method-restrictor.js
+++ src/middleware/method-restrictor.js
@@ -1,6 +1,23 @@
+const logger = require('../utils/logger');
+
 const methodRestrictor = {
-    badRequest: (req, res) => res.status(400).send(),
-    methodNotAllowed: (req, res) => res.status(405).send()
+  badRequest: (req, res) => {
+    logger.warn('400 Bad Request', {
+      method: req.method,
+      url: req.originalUrl,
+      ip: req.ip
+    });
+    res.status(400).send();
+  },
+
+  methodNotAllowed: (req, res) => {
+    logger.warn('405 Method Not Allowed', {
+      method: req.method,
+      url: req.originalUrl,
+      ip: req.ip
+    });
+    res.status(405).send();
+  }
 };
 
 module.exports = methodRestrictor;
